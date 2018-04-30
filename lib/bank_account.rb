b7c495a28dcd8d16a27ef3db400b3ea9371623ac
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-30 03:46:13 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,11 @@
 class BankAccount
 
-  attr_reader :name, :status 
+  attr_reader :name, :status, :balance
 
-  def initialize(name, status = 'open')
+  def initialize(name, status = 'open', balance = 1000)
    @name = name
    @status = status
+   @balance = balance
    end
 
 end

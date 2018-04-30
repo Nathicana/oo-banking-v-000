c0edd535b5d22dac6b44fd978e10fbdb95f8553c
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-30 04:41:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,15 +26,16 @@ def execute_transaction
 end
 
 
-def reverse_transfer 
+def reverse_transfer
  if valid? && sender.balance > amount && self.status == "complete"
-  receiver.balance -= amount 
-  sender.balance += amount 
+  receiver.balance -= amount
+  sender.balance += amount
   self.status = "reversed"
-else 
-  transfer_rejected 
-endend
-  
+else
+  reject_transfer
+end
+end
+
 
 def reject_transfer
   self.status = "rejected"
